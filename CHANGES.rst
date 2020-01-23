3905d157902444155f9a6ee40de8f44015f275ae
Laurent Lasudry <lasudry@gmail.com>
2020-01-23 10:05:41 +0100

    Preparing release 1.0.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-1.0.1 (unreleased)
+1.0.1 (2020-01-23)
 ------------------
 
 - Let Plone mark_special_links pattern do the work for opening links to external
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.iconifiednavigation',
-    version='1.0.1.dev0',
+    version='1.0.1',
     description="Add content icons in portal_tabs menu",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
